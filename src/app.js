487b382b243cf1a7b7a7ebaded6b1e2ba511b8c1
aufaahusniati <aufaahsnt21@gmail.com>
2024-04-24 12:54:03 +0700

    proyek akhir

---
+++ src/app.js
@@ -0,0 +1,127 @@
+import "./styles/style.css";
+import "./script/components/index.js";
+import { animate, glide } from "motion";
+
+const baseUrl = "https://notes-api.dicoding.dev/v2";
+
+const getNote = () => {
+  fetch(`${baseUrl}/notes`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((responseJson) => {
+      if (responseJson.error) {
+        showResponseMessage(responseJson.message);
+      } else {
+        console.log(responseJson.data);
+        renderNotes(responseJson.data);
+      }
+    })
+    .catch((error) => {
+      showResponseMessage(error);
+    });
+};
+
+const addNote = (note) => {
+  showLoadingIndicator();
+  fetch(`${baseUrl}/notes`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Auth-Token": "12345",
+    },
+    body: JSON.stringify({ title: note.title, body: note.body }),
+  });
+  Swal.fire({
+    position: "center",
+    icon: "success",
+    title: "Notes has been added",
+    showConfirmButton: false,
+    timer: 1500,
+  })
+    .then((responseJson) => {
+      hideLoadingIndicator();
+      getNote();
+    })
+    .catch((error) => {
+      hideLoadingIndicator();
+      showResponseMessage(error);
+    });
+};
+
+const removeNote = (noteId) => {
+  fetch(`${baseUrl}/notes/${noteId}`, {
+    method: "DELETE",
+    headers: {
+      "X-Auth-Token": "12345",
+    },
+  });
+  Swal.fire({
+    title: "Deleted!",
+    text: "Your note has been deleted.",
+    icon: "success",
+  })
+    .then((responseJson) => {
+      getNote();
+    })
+    .catch((error) => {
+      hideLoadingIndicator();
+      showResponseMessage(error);
+    });
+};
+
+const renderNotes = (notes) => {
+  const noteItems = document.querySelectorAll("note-item");
+  noteItems.innerHTML = "";
+
+  noteItems.forEach((noteItem) => {
+    noteItem.note = notes;
+    const shadowRoot = noteItem.shadowRoot;
+
+    const cardNotes = shadowRoot.querySelectorAll(".note-delete");
+    cardNotes.forEach((cardNote) => {
+      const deleteButton = cardNote.querySelector(".button-delete");
+      if (deleteButton) {
+        deleteButton.addEventListener("click", (event) => {
+          const noteId = event.target.dataset.id;
+
+          removeNote(noteId);
+        });
+      }
+    });
+  });
+};
+
+const showResponseMessage = (message) => {
+  Swal.fire({
+    title: "The Internet?",
+    text: "That thing is still around?",
+    icon: "question",
+  });
+  showResponseMessage(message);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  getNote();
+
+  const inputNoteElement = document.querySelector("note-input");
+  if (inputNoteElement) {
+    inputNoteElement.addEventListener("note-added", (event) => {
+      const newNote = event.detail;
+      addNote(newNote);
+    });
+  }
+});
+
+const showLoadingIndicator = () => {
+  const loadingIndicator = document.querySelector(".loadingIndicator");
+  loadingIndicator.style.display = "flex";
+};
+
+const hideLoadingIndicator = () => {
+  const loadingIndicator = document.querySelector(".loadingIndicator");
+  loadingIndicator.style.display = "none";
+};
+
+// animasi
+animate("#itemNotes", { y: 0 }, { easing: glide({ velocity: -50 }) });
---
+++ src/script/components/note-item.js
@@ -0,0 +1,129 @@
+class notesItem extends HTMLElement {
+  _shadowRoot = null;
+  _style = null;
+  _note = {
+    id: null,
+    title: null,
+    body: null,
+    createdAt: null,
+  };
+
+  constructor() {
+    super();
+
+    this._shadowRoot = this.attachShadow({ mode: "open" });
+    this._style = document.createElement("style");
+  }
+
+  _emptyContent() {
+    this._shadowRoot.innerHTML = "";
+  }
+
+  set note(value) {
+    this._note = value;
+
+    this.render();
+  }
+
+  get note() {
+    return this._note;
+  }
+
+  _updateStyle() {
+    this._style.textContent = `
+        :host {
+          display: block;
+        }
+
+        .grid-wrapper {
+          padding-top: 5%;
+          display: grid;
+        }
+
+        .grid-wrapper .all-notes {
+            padding-top: 1em;
+            text-align: center;
+        }
+
+        hr {
+            width: 20%;
+            border: 1.8px solid #DE9D7E;
+        }
+      
+        .grid-wrapper .grid-container {
+            display: grid;
+            grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+            align-items: center;
+            justify-items: center;
+            gap: 2em;
+            padding: 2em;
+        }
+        
+        .grid-container .card-note {
+            background-color: white;
+            opacity: 0.8;
+            border: none;
+            border-radius: 10px;
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+            width: 90%;
+            height: 90%;
+            padding: 1em;
+        }
+
+        .card-note .date {
+          font-size: 0.70em;
+          margin-top: -9px;
+        }
+        
+        .card-note .desc {
+              padding-top: 1em;
+              font-size: 0.90em;
+          }
+
+        .card-note .note-delete {
+            padding-top: 1em;
+        }
+
+        .note-delete .button-delete {
+            background-color: red;
+            color: white;
+            border: none;
+            border-radius: 5px;
+            padding: 8px;
+            cursor: pointer;
+        }
+    `;
+  }
+
+  render() {
+    this._emptyContent();
+    this._updateStyle();
+
+    this._shadowRoot.appendChild(this._style);
+    this._shadowRoot.innerHTML += `
+
+          <div class="grid-wrapper">
+            <h1 class="all-notes">All Notes</h1><hr>
+            <div class="grid-container">
+            ${this._note
+              .map(
+                (note) => `
+                <div class="card-note">
+                    <h4>${note.title}</h4>
+                    <p class="date">${new Date(note.createdAt).toLocaleString()}</p>
+                    <p class="desc">${note.body}</p>
+
+                    <div class="note-delete">
+                        <button class="button-delete" type="button" data-id="${note.id}" >Delete</button>
+                    </div>
+                </div>
+                `,
+              )
+              .join("")}
+            </div>
+        </div>
+    `;
+  }
+}
+
+customElements.define("note-item", notesItem);
